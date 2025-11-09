84c006a21a5baaf7e910e2a8d4e97d1714ecfac3
Sambhav-3010 <sambhavmani.tripathi@gmail.com>
2025-11-09 20:40:01 +0530

    Switched to nenon database

--- Backend/app/server.ts
+++ Backend/app/server.ts
@@ -1,16 +1,37 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { neon } from "@neondatabase/serverless";
+
 dotenv.config();
 
 const app = express();
-const PORT: number = 3000;
+const PORT: number = Number(process.env.PORT) || 5000;
+const sql = neon(process.env.DATABASE_URL as string);
 
-app.get('/', (req, res) => {
-    res.send('Welcome to splitwise backend!');
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL as string],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Splitwise backend!");
 });
 
-app.listen(PORT, () => {
-    console.log('The application is listening on port http://localhost:' + PORT);
-})
+app.listen(PORT, async () => {
+  try {
+    const result = await sql`SELECT NOW()`;
+    console.log("Connected to neon database");
+    console.log(`Server running at http://localhost:${PORT}`);
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+  }
+});
